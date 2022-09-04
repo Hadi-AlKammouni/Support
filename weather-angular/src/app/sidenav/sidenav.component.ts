90809dacbdb2751c03906e1b17cf1599c92240f5
Hadi-AlKammouni <hadi-kam99@hotmail.com>
2022-09-04 22:18:00 +0300

    angular/Giving an active class to the active component

--- weather-angular/src/app/sidenav/sidenav.component.ts
+++ weather-angular/src/app/sidenav/sidenav.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, HostListener, OnInit, Output } from '@angular/core';
 
 import { navbarData } from './nav-data';
 
@@ -14,14 +14,25 @@ interface SidenavToggle {
 export class SidenavComponent implements OnInit {
 
   @Output() onToggleSidenav: EventEmitter<SidenavToggle> = new EventEmitter()
-
+  // @HostListener('window:resize', ['$event'])
+  
   collapsed = false
   navData = navbarData
   screenWidth = 0
-
+  
   constructor() { }
-
+  
+  // onResize(event: any) {
+  //   this.screenWidth = window.innerWidth
+  //   console.log(this.screenWidth)
+  //   if (this.screenWidth <= 768) {
+  //     this.collapsed = false
+  //     this.onToggleSidenav.emit({collapsed: this.collapsed, screenWidth: this.screenWidth})
+  //   }
+  // }
+  
   ngOnInit(): void {
+    this.screenWidth = window.innerWidth
   }
 
   toggleCollapse() {
