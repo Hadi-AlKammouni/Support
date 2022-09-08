386f197e7f5096eb2fa2b9462f2e5d0063398457
Hadi-AlKammouni <hadi-kam99@hotmail.com>
2022-09-08 08:44:08 +0300

    laravel/Implementing login functionality

--- support-laravel/app/Http/Controllers/JWTController.php
+++ support-laravel/app/Http/Controllers/JWTController.php
@@ -49,4 +49,31 @@ class JWTController extends Controller
             'user' => $user
         ], 201);
     }
+
+    /**
+     * login user
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function login(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required|string|min:6',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        if (!$token = auth()->attempt($validator->validated())) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth('api')->factory()->getTTL() * 60
+        ], 200);
+    }
 }
