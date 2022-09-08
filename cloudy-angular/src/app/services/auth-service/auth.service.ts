2b466b3fd26f49dcacaf5df207a7d767add84f9b
Hadi-AlKammouni <hadi-kam99@hotmail.com>
2022-09-08 09:28:49 +0300

    angular/Sending register request

--- cloudy-angular/src/app/services/auth-service/auth.service.ts
+++ cloudy-angular/src/app/services/auth-service/auth.service.ts
@@ -1,9 +1,24 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { Register } from 'src/app/models/register.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  register(name: string, email: string, password: string, confirmPassword: string): Observable<Register> {
+    return this.http.post<Register>('http://localhost:8000/api/register',
+      {
+        "name": name,
+        "email": email,
+        "password": password,
+        "password_confirmation": confirmPassword
+      })
+  }
+
 }
